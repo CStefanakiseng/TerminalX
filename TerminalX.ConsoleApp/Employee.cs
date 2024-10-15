b0569d8e26e390f21c88e4b23b52914d87991fc6
c.stefanakis <c.stefanakis@dataverse.gr>
2024-10-15 13:46:54 +0300

    Automapper

---
+++ TerminalX.ConsoleApp/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace TerminalX.ConsoleApp;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public string Address { get; set; }
+    public string Department { get; set; }
+}
+
+public class EmployeeDTO
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public string Address { get; set; }
+    public string Department { get; set; }
+}
--- TerminalX.ConsoleApp/Program.cs
+++ TerminalX.ConsoleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+using TerminalX.ConsoleApp;
+
 Console.WriteLine("Hello, World! v1");
+
+//Create and Populate Employee Object i.e. Source Object
+Employee emp = new Employee
+{
+    Name = "James",
+    Salary = 20000,
+    Address = "London",
+    Department = "IT"
+};
+
+//Initializing AutoMapper
+var mapper = MapperConfig.InitializeAutomapper();
+//Way1: Specify the Destination Type and to The Map Method pass the Source Object
+//Now, empDTO1 object will having the same values as emp object
+var empDTO1 = mapper.Map<EmployeeDTO>(emp);
+
+//Way2: Specify the both Source and Destination Type 
+//and to The Map Method pass the Source Object
+//Now, empDTO2 object will having the same values as emp object
+var empDTO2 = mapper.Map<Employee, EmployeeDTO>(emp);
+Console.WriteLine("Name: " + empDTO1.Name + ", Salary: " + empDTO1.Salary + ", Address: " + empDTO1.Address + ", Department: " + empDTO1.Department);
+Console.ReadLine();
